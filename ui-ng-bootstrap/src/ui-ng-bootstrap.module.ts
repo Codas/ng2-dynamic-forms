d462ea4ddd3bcc5071b3d2638a9c2ccf222ad744
Lars Gröber <larsgroeber7@gmail.com>
2017-10-23 16:31:10 +0200

    fixed error in ui-ng-bootstrap.module.ts

--- ui-ng-bootstrap/src/ui-ng-bootstrap.module.ts
+++ ui-ng-bootstrap/src/ui-ng-bootstrap.module.ts
@@ -6,7 +6,7 @@ import { NgbDatepickerModule, NgbButtonsModule, NgbTimepickerModule, NgbTypeahea
 import { TextMaskModule } from "angular2-text-mask";
 import { DynamicFormNGBootstrapComponent } from "./dynamic-form-ng-bootstrap.component";
 import { NgbTagTypeaheadComponent } from './ngb-tag-typeahead.component';
-import { NgbFileUploadComponent } from './ngb-file-upload/ngb-file-upload.component';
+import { NgbFileUploadComponent } from './ng2-file-upload/ng2-file-upload.component';
 import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
